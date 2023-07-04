eeff226ef7996e215cb0a8f68532ac78d0c5acbf
test <test@test.com>
2023-07-04 18:44:39 +0900

    수업

---
+++ src/main/java/com/quiz/lesson07/entity/CompanyEntity.java
@@ -0,0 +1,43 @@
+package com.quiz.lesson07.entity;
+
+import java.time.ZonedDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString // toString override 객체값 예쁘게 출력
+@AllArgsConstructor // 모든 필드를 인자값으로 받는 생성자 (파라미터 있는 생성자)
+@NoArgsConstructor // 파라미터 없는 기본 생성자
+@Builder // setter 대용
+@Getter // getter
+@Table(name = "company") // 테이블명에 언더바 붙어있을 때 꼭 추가한다.
+@Entity // 이 클래스는 Entity다
+public class CompanyEntity {
+	@Id // pk
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
+	private int id;
+	private String name;
+	private String business;
+	private String scale;
+	private int headcount;
+	
+	@UpdateTimestamp // createdAt이 null이면 현재 시간으로 자동으로 입력
+	@Column(name = "createdAt", updatable = false) // updatable = false : 업데이트시 시간 갱신 안함
+	private ZonedDateTime createdAt;
+	
+	@UpdateTimestamp // updatedAt이 null이면 현재 시간으로 자동으로 입력
+	@Column(name = "updatedAt")
+	private ZonedDateTime updatedAt;
+}
